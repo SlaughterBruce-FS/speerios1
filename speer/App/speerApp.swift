152e86a860904766bcd3b781730a7c319ea23c8c
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2024-01-02 23:07:13 -0500

    Initial Commit

---
+++ speer/App/speerApp.swift
@@ -0,0 +1,17 @@
+//
+//  speerApp.swift
+//  speer
+//
+//  Created by J C on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct speerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
